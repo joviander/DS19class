bac5a3af6db70cd05e41f1937d8608d0083fdc22
joviander <janderson@bendoregon.gov>
2019-04-05 11:38:47 -0700

    added summarize functions

--- code/intro-dplyr.R
+++ code/intro-dplyr.R
@@ -44,9 +44,78 @@ africa <- select(africa,country, population = pop, lifeExp)
 #select year, population, country, for europe
 europe <- gapminder %>%
     filter(continent == "Europe") %>%
-  select(year,population = pop, country)
+  select(year, population = pop, country)
 europe_table <- table(europe$country)
 View(europe_table)
 
+#working with group_by() and summarize() replace pivot tables
+
+str(gapminder %>% group_by(continent))
+
+#summarize gdp by continent
+gdp_continent <-  gapminder %>%
+  group_by(continent)%>%
+  summarize(mean_gdp = mean(gdpPercap))
+
+View(gdp_continent)
+
+#put in chart needs help (mean_lifeExp not found)
+
+library(ggplot2)
+summary_plot <- gdp_continent %>%
+  ggplot(aes(x = mean_gdp, y = mean_lifeExp)) +
+  geom_point(stat = "identity") +
+  theme_bw()
+summary_plot
+
+#calculate mean population for all the continents
+
+gdp_continent <-  gapminder %>%
+  group_by(continent)%>%
+  summarize(mean_pop = mean(pop))
 
 
+# sqrt details
+gapminder %>%
+  group_by(continent) %>%
+  summarize(se = sd(lifeExp)/sqrt(n()))
+
+#mutate() is my friend??? diff from summarize, add a new column, 
+#to make a calculation and a new outut in your dataframe
+
+xy <- data.frame(x = rnorm(100),
+                 y = rnorm(100))
+head(xy)
+xyz <- xy %>%
+  mutate(z = x*y)
+head(xyz)
+  
+#add a column that gives full gdp per country and then by continent
+total_gdp_country <- gapminder %>%
+  mutate(totalgdp = pop*gdpPercap)
+head(total_gdp_country)
+
+
+gdp_per_cont <- gapminder%>%
+  mutate(total_gdp = pop*gdpPercap) %>%
+  group_by(continent) %>%
+  summarise(cont_gdp = sum(total_gdp))
+gdp_per_cont
+
+
+
+
+#another way same detail as above
+gdp_per_cont <- total_gdp_country %>%
+  group_by(continent)  %>%
+  summarise(cont_gdp = sum(total_gdp))
+gdp_per_cont
+
+  
+
+
+
+
+
+
+  
